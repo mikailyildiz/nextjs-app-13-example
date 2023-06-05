e861cfca6315107615795859b4ee289c4554df58
Mikail <mikailyildiz@hotmail.com>
2023-06-05 11:19:54 +0300

    remove console log and eslint config fix

--- app/lib/getProduct.ts
+++ app/lib/getProduct.ts
@@ -2,7 +2,6 @@ export default async function getProduct(id: number) {
     //const headersInstance = headers();
     //const authorization = headersInstance.get('authorization');
     // Forward the authorization header
-    console.log("istek", id)
     const res = await fetch(`https://fakestoreapi.com/products/${id}`);
 
     return res.json();
