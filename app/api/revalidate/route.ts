814ad4926d454aa830466e55640dd02bbeec04e2
Mikail <mikailyildiz@hotmail.com>
2023-06-06 15:26:52 +0300

    revalidateTag name fix

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -6,6 +6,6 @@ export async function GET(request: NextRequest) {
 
   const tag:string = request.nextUrl.searchParams.get('tag') || ''
 
-  revalidatedTag(tag)
+  revalidateTag(tag)
   return NextResponse.json({revalidated: true, now: Date.now()})
 }
