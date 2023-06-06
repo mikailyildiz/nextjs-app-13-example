1d0bed24f66d15239ee1672442b0f5954c6a383f
Mikail <mikailyildiz@hotmail.com>
2023-06-06 15:32:45 +0300

    Dynamic Data Fetching

--- app/lib/getProduct.ts
+++ app/lib/getProduct.ts
@@ -2,7 +2,9 @@ export default async function getProduct(id: number) {
     //const headersInstance = headers();
     //const authorization = headersInstance.get('authorization');
     // Forward the authorization header
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`, 
+    //{cache: 'no-store'}
+    );
 
     return res.json();
   }
