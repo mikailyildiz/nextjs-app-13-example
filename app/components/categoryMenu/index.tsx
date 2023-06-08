7a789b9e859a9b2d96363ea8fa931d0b653eb5fa
Mikail <mikailyildiz@hotmail.com>
2023-06-08 15:51:03 +0300

    Handling Errors in Root Layouts

--- app/components/categoryMenu/index.tsx
+++ app/components/categoryMenu/index.tsx
@@ -7,7 +7,7 @@ export default async function CategoryMenu({category}:{category: string}) {
 
   return (
     <>
-      {/* <h3>{categories[99].title}</h3> */}
+      <h3>{categories[99].title}</h3>
       {categories.map((item: string, index: number) => (
         <LinkButton selectedCategory={category} key={index} text={item} url={`/products/${encodeURIComponent(item)}`}/>
       )) }
