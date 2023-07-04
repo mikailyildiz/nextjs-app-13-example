7d92dac67beedb03a390ce6b8c3fab57783df752
Mikail <mikailyildiz@hotmail.com>
2023-07-04 15:53:33 +0300

    summary unit test Coverage

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,12 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{jsx,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
 }
 
 module.exports = createJestConfig(customJestConfig)
