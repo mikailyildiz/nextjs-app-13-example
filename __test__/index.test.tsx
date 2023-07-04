c9d35d45c96f185da0407b458879c76af59ac160
Mikail <mikailyildiz@hotmail.com>
2023-07-04 15:47:21 +0300

    component add all variant test

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,14 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import AddBookmark from '../app/[lang]/components/addBookmark'
 
 describe('AddBookmark', () => {
-  it('renders a AddBookmark', () => {
-    const { getByTestId, getByText } = render(
-      <AddBookmark selected={false} onBookmark={() => {}} />
-    )
+  it('should render bookmark icon', () => {
+    const { getByTestId } = render(<AddBookmark selected={false} onBookmark={() => {}} />);
+    const bookmarkIcon = getByTestId('add-bookmark');
+    expect(bookmarkIcon).toBeInTheDocument();
+  });
 
-    const addBookmarkContainer = getByTestId('add-bookmark')
+  it('should add selected class when selected prop is true', () => {
+    const { getByTestId } = render(<AddBookmark selected={true} onBookmark={() => {}} />);
+    const bookmarkIcon = getByTestId('add-bookmark');
+    expect(bookmarkIcon).toHaveClass('selected');
+  });
 
-    expect(addBookmarkContainer).toBeInTheDocument()
-  })
-})
+  it('should not have selected class when selected prop is false', () => {
+    const { getByTestId } = render(<AddBookmark selected={false} onBookmark={() => {}} />);
+    const bookmarkIcon = getByTestId('add-bookmark');
+    expect(bookmarkIcon).not.toHaveClass('selected');
+  });
+
+  it('should call onBookmark function when bookmark is clicked', () => {
+    const onBookmarkMock = jest.fn();
+    const { getByTestId } = render(<AddBookmark selected={false} onBookmark={onBookmarkMock} />);
+    const bookmarkIcon = getByTestId('add-bookmark');
+    fireEvent.click(bookmarkIcon);
+    expect(onBookmarkMock).toHaveBeenCalled();
+  });
+});
